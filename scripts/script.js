8088cdc5b5b46312d1c9f2162cb13a0e75a1f090
yagowill <99210475+yagowill@users.noreply.github.com>
2022-02-28 21:12:13 -0300

    update links

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
 function perfil() {
     let user = document.getElementById('user').value
     let img = document.getElementById('perfil')
-        if (user == 'iron tony') {
-            img.src = 'imagens/iron-perfil.png'
-        } else if (user == 'cap steve') {
-            img.src = 'imagens/cap-perfil.png'            
-        } else {
-            img.src = 'imagens/shield.png'
-        }
-      
+    if (user == 'iron tony') {
+        img.src = 'imagens/iron-perfil.png'
+    } else if (user == 'cap steve') {
+        img.src = 'imagens/cap-perfil.png'
+    } else {
+        img.src = 'imagens/shield.png'
     }
+
+}
 function logIn() {
     user = document.getElementById('user').value
     let senha = document.getElementById('pass').value
     if (user == 'iron tony' && senha == 'iloveu3000') {
-    location.href = "https://yagowill.github.io/projeto-shield/iron.html"
-    } else if (user == 'cap steve' && senha == 'peggy' ) {
-    location.href = "https://yagowill.github.io/projeto-shield/cap.html"
-    }else {
+        location.src = "iron.html"
+    } else if (user == 'cap steve' && senha == 'peggy') {
+        location.src = "cap.html"
+    } else {
         alert('Usuário ou senha incorreto!')
     }
 }
